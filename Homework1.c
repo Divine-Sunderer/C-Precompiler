65874e2da669d4984148b376033e7086033ea717
Aiden <salvati.2050590@studenti.uniroma1.it>
2025-04-09 09:56:01 +0200

    Create Homework1.c

---
+++ Homework1.c
@@ -0,0 +1,78 @@
+//Piccolo programma di prova ( non testato ) per vedere se un file si apre correttamente
+
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+bool checkFile(FILE * prova)
+{
+    if( prova )
+    {
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char * argv[]) { //argc e argv ci permettono di inserire comandi dal prompt
+    int char_pointer;
+    char riga[256]; // Vettore di caratteri per contenere una riga
+    char carattere;
+    FILE *prova;
+    prova = fopen("C:\\Users\\cripo\\Desktop\\sistemi operativi II\\test1\\test1\\test1.c", "w+");
+    // w+ apre il file in lettura e scrittura. Anche r+ fa lo stesso , ma senza andarlo a creare se inesistente
+
+    if (checkFile(prova)) {
+        printf("File aperto correttamente ");
+    } else {
+        perror("File inesistente!");
+        return 0;
+    }
+
+    //fprintf(prova, "paperella gay sei l'uccello che vorrei \n");
+    rewind(prova); // riporta il puntatore all'inizio
+
+    // Cicla le righe
+    while (fgets(riga, sizeof(riga), prova) != NULL) {
+
+        if (strstr(riga, "//") != NULL) {
+            for ( int i = 0 ; i < strlen(riga) ; i++ ) {
+                if (riga[i] == '/' && riga[i+1] == '/') {
+                    char_pointer = i;
+
+                    while ( char_pointer < strlen(riga) ) {
+                        riga[char_pointer] = ' ';
+                        char_pointer++;
+                    }
+                    break;
+                }
+            }
+        }else if (strstr(riga, "/*") != NULL) {
+            for ( int i = 0 ; i < strlen(riga) ; i++ ) {
+                if ( riga[i] == '/' && riga[i+1] == '*' ) {
+                    char_pointer = i;
+
+                    while ( char_pointer < strlen(riga) || (riga[i] != '*' && riga[i+1] != '/')) {
+                        riga[char_pointer] = ' ';
+                        char_pointer++;
+                    }
+                }
+            }
+        }else if (strstr(riga,"*/")) {
+            for ( int i = 0 ; i < strlen(riga) ; i++ ) {
+                if ( riga[i] != '*' && riga[i+1] != '/') {
+                    char_pointer = i;
+
+                    while ( char_pointer < strlen(riga) || /*Aggiungere la condizione che fa cancellare fino ad un carattere dopo -> */) {
+                        riga[char_pointer] = ' ';
+                    }
+                }
+            }
+        }
+
+
+
+        printf("Letto : %s", riga);
+    }
+
+    return 0;
+}
