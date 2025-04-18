baeb6482fd3dd1816fea3ab6aa3feb5c527c4bfd
Aiden <salvati.2050590@studenti.uniroma1.it>
2025-04-18 17:15:23 +0200

    Ampliata la funzione che controlla i vari tipi dell'eventuale file.c inserito. C'è ancora tanto da inserire. Alla riga 90 trovi l'idea per sistemare migliorare ulteriormente il codice. Inoltre ho fixato due "piccoli" errori che mi stavo portando dietro ( non ancora testato ).

--- controllo_variabili.c
+++ controllo_variabili.c
@@ -12,28 +12,59 @@ bool checkFile(FILE * prova)
     return false;
 }
 
-void controllo_variabili(char *riga[],char *tipo[]) {
-    int punta_carattere = 0;
+bool checkChar(char nome_var[]) {
+  char str_vietata[] = " !\"#%&'()*+,-./:;<=>?@[\\]^`{|}~$@€£§¶©®™✓°•½¼¾µ¤¿¡¨¸ªº×÷±¬ƒ¬„…‰†‡‹›";
+  for(int i = 0; i < strlen(str_vietata); i++) {
+        if(strchr(nome_var, str_vietata[i])) {                                                                          //Check se il singolo carattere si trova nel nome
+          return true;
+        }
+    }
+    return false;
+  }
+
+bool isNum(char carattere) {
+      if(carattere >= '0' && carattere <= '9')
+        {
+            return true;
+        }
+        return false;
+  }
+
+int controllo_variabili(char riga[], char tipo[], int index_file) {
+    int punta_char = 0 , conta_errori = 0;
     char nome_var[50];
 
-      if(strstr(*riga,*tipo) != NULL)
+      if(strstr(riga,tipo) != NULL)
         {
-          for(int i = 0; i < strlen(*riga); i++)
+          for(int i = 0; i < strlen(riga); i++)
           {
-              if(*riga[i] == ' ')
+              if(riga[i] == ' ')
                   continue;
 
-              if(*riga[i] == 'i' && *riga[i+1] == 'n' && *riga[i+2] == 't' && *riga[i+3] == ' ')
+              if( riga[i] == 'i' && riga[i+1] == 'n' && riga[i+2] == 't' && riga[i+3] == ' ' )
                 {
-                    i = i + 4; // Mando avanti di 4 caratteri ( cosi arrivo al nome della variabile )
-
+                    i = i + 4;                                                                                          // Mando avanti di 4 caratteri ( cosi arrivo al nome della variabile )
+                    punta_char = i;
+                    while(riga[i] != ';' && riga[i] != '\0')
+                      {
+                          i++;
+                      }
+                      strncpy(nome_var , riga + punta_char, (i - punta_char));
+                      nome_var[i - punta_char] = '\0';                                                                  //Chiudo la stringa manualmente
+                      if( isNum(nome_var[0]) || checkChar(nome_var) )
+                        {
+                            conta_errori++;
+                            printf("Errore nome_var alla riga : %d , non consentito",index_file);
+                        }
                 }
           }
         }
+        return conta_errori;
   }
 
 int main_controllo_variabili() {
   int conta_errori = 0;
+  int index_file = 1;
   char riga[256];
 
   FILE *input;
@@ -47,18 +78,21 @@ int main_controllo_variabili() {
     }
 
     while(fgets(riga, sizeof(riga), input) != NULL) {
-      controllo_variabili(riga,"int");
-      /*if(strstr(riga,controllo_variabili(riga,"int")) != NULL)
-        {
-            for(int i = 0; i < strlen(riga); i++)
-              {
-                  if(riga[i] == ' ')
-                    continue;
+      conta_errori = conta_errori + controllo_variabili(riga,"int",index_file);
 
-                  if(
-              }
-        }*/
+      index_file++;
     }
   return 0;
-  }
 }
+
+//IDEE
+/*
+*  Nella riga 44 ( circa , non lo so ) dovremmo in teoria inserire UNA MAREA di casi diversi
+*  Ma inserirli tutti li è brutto visivamente , cosi stavo pensando di creare una funziona secondaria
+*  Il problema è che non so se sia fattibile farla con una sfilza di if oppure se uno switch sia possibile
+*  inserirlo... Non so , idee ?
+*
+*  https://en.wikipedia.org/wiki/C_data_types
+*
+*  Qui sopra ci sono tutti i tipi che vanno inseriti
+ */
