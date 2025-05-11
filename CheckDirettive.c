bcf0895ed260c789831488f876958f3acc0e42f6
Aiden <salvati.2050590@studenti.uniroma1.it>
2025-05-11 19:29:24 +0200

    Inserita anche la copiatura del contenuto del file nel nuovo. Ho utilizzato 3 file in totale. Uno per il file originale. L'altro per i file include e l'ultimo come nuova copia. Mai scrivere nel file originale :c

--- CheckDirettive.c
+++ CheckDirettive.c
@@ -7,30 +7,32 @@
 // ruturn 2 -> errore inerente a malloc
 
 int main() {
-  char riga[256];
+  char riga[256] , raw[256];
   char *str_din , *inizio , *fine;
   bool start = false;
   int count_str = 0;
-  FILE *principale , *dir;
+  FILE *principale , *dir, *nuovo;
 
   principale = fopen("prova.txt", "r+");
+  nuovo = fopen("nuovo_prova_1.txt", "w+");
   if (!principale) return 1;
+  if (!nuovo) return 1;
 
   while(fgets(riga, sizeof(riga), principale) != NULL){
       //Se trova la sotto-stringa "#include" dentro la stringa
       if(strstr(riga,"#include") && strchr(riga,'\"')){
         for(int i = 0; i < strlen(riga); i++){
-          if(riga[i] == '\"' && start == false){            //Inizia la conta dei caratteri per l'allocazione dinamica
-            count_str = 0;                                  //Va azzerato altrimenti allochiamo troppa memoria inutilmente
+          if(riga[i] == '\"' && start == false){                                                                        //Inizia la conta dei caratteri per l'allocazione dinamica
+            count_str = 0;                                                                                              //Va azzerato altrimenti allochiamo troppa memoria inutilmente
             start = true;
             inizio = strchr(riga+i, '\"') + 1;
             count_str++;
-          }else if(riga[i] == '\"' && start == true){          //Finisce la conta dei caratteri per l'allocazione dinamica
+          }else if(riga[i] == '\"' && start == true){                                                                   //Finisce la conta dei caratteri per l'allocazione dinamica
             start = false;
             fine = strchr(inizio, '\"');
             break;
           }
-          if(start){              //Finché start == true ( e non trova nessun'altra virgoletta )  incrementa il conteggio
+          if(start){                                                                                                    //Finché start == true ( e non trova nessun'altra virgoletta )  incrementa il conteggio
             count_str++;
           }
         }
@@ -44,10 +46,19 @@ int main() {
 
         strncpy(str_din, inizio, fine - inizio);
         str_din[fine - inizio] = '\0';              //Altrimenti non avrebbe fine
-        //TODO Ricorda di fare free(str_din)
+
         dir = fopen(str_din, "r+");
 
-        //Adesso manca la copiatura del file
+        while(fgets(raw, sizeof(raw), dir) != NULL){
+          fprintf(nuovo,"%s", raw);
+        }
+        fclose(dir);
+        free(str_din);
+      }else{
+        fprintf(nuovo,"%s",riga);
       }
   }
+  fclose(nuovo);
+  fclose(principale);
+  return 0;
 }
