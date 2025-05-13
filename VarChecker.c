82622465795545f4301e9bd925f6efec4bdc167d
Aiden <salvati.2050590@studenti.uniroma1.it>
2025-05-13 11:37:57 +0200

    Eliminati numerosi bug ma ancora instabile. Riconosce ancora il nome delle funzioni come delle variabili. Fa creata una nuova funzione per eliminare questo problema.

--- VarChecker.c
+++ VarChecker.c
@@ -3,11 +3,23 @@
 #include<string.h>
 #include<stdbool.h>
 
+const char *parole_riservate[] = {
+  "break", "case", "continue", "default", "do",
+  "else", "for", "goto", "if", "inline", "restrict",
+  "return", "sizeof", "switch", "while",
+};
+
+#define len_res (sizeof(parole_riservate)/sizeof(parole_riservate[0]))                                                  //Lunghezza array di stringhe
+
 int isValidName(char *var) {
   if (!var || (!isalpha(var[0]) && var[0] != '_')) return 0;
 
   for (int i = 1; var[i]; i++) {
     if (!isalnum(var[i]) && var[i] != '_') return 0;
+  }
+
+  for (int i = 0; i < len_res; i++) {
+    if (strstr(var, parole_riservate[i])) return 0;
   }
 
   return 1;
@@ -16,7 +28,25 @@ int isValidName(char *var) {
 char* eliminaSpaziIniziali(char str[]) {
   int i = 0;
   while (str[i] == ' ') i++;  // salta spazi iniziali
-  return &str[i];             // ritorna puntatore alla prima lettera non spazio
+  return &str[i];             // ritorna puntatore alla prima lettera dopo gli spazi
+}
+
+//Per "tappare" il bug dei /tab all'inizio del nome delle variabili
+char* extendedTrim(char *str) {
+  while (*str && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r'))
+    str++;
+
+  char *end = str + strlen(str) - 1;
+  while (end > str && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r')) {
+    *end = '\0';
+    end--;
+  }
+  return str;
+}
+
+char* charCleaner(char *str) {
+  if (str[strlen(str) - 1] == ';') str[strlen(str) - 1] = '\0';
+  return str;
 }
 
 int main() {
@@ -30,83 +60,70 @@ int main() {
   //char stringa[] = "int pippo, paperino = 3;";
   //char stringa[] = "int ciao;";
   char stringa[256];
-/*
-  const char* tipo[] = {
-    "auto", "break", "case", "char", "const", "continue", "default", "do",
-    "double", "else", "enum", "extern", "float", "for", "goto", "if",
-    "int", "long", "register", "return", "short", "unsigned", "sizeof", "static",
-    "struct", "switch", "typedef", "union", "signed", "void", "volatile", "while"
-};*/
-
   const char *tipo[] = {
     "int","char","float","double","void","short","long","unsigned","signed","auto",
     "register","static","extern","const","volatile","struct","union","enum","typedef"
-};
+  };
 
   const int tipo_lenght = sizeof(tipo) / sizeof(tipo[0]);
-  principale = fopen("test1_no_comment.c", "r+");
+  principale = fopen("test2.c", "r+");
   if (!principale) return 1;
 
   while (fgets(stringa, sizeof(stringa), principale)) {
-    trovato = false;
+      index_variabili = 0;
+      trovato = false;
 
-    for (int i = 0; i < tipo_lenght; i++) {
-      if (strstr(stringa, tipo[i])) {
-        trovato = true;
-        break;
-      }
-    }
-    if (!trovato) continue;
-
-    for (int i = 0; i < tipo_lenght; i++) {
-      char *pos = strstr(stringa, tipo[i]);                                                                     //Cerchiamo il tipo come sotto-stringa
-
-    //Se lo trova allora potrei far fare i-- , cosi che ricontrolli nuovamente per lo stesso tipo se ce ne è un'altra
-      if (pos != NULL) {
-        index = pos - stringa;                                                                                            //Calcoliamo l'indice sottraendo i puntatori
-        for (int j = 0; j < strlen(tipo[i]); j++) {
-          stringa[index++] = ' ';
+      for (int i = 0; i < tipo_lenght; i++) {
+        if (strstr(stringa, tipo[i])) {
+          trovato = true;
+          break;
         }
-        i--;
       }
-  }
-  //printf("%s\n",stringa);
-  char *finale = eliminaSpaziIniziali(stringa);
-  //printf("%s\n",finale);
+      if (!trovato) continue;
 
-  if (strchr(finale, '=')) {
-    for (int i = strlen(finale); finale[i] != '=' ; i--) {
-      finale[i] = ' ';
-      index = i;
-  }
-    finale[index-1] = '\0';
-  }else{
-    index = strlen(finale);
-    finale[index-1] = '\0';
-  }
+      for (int i = 0; i < tipo_lenght; i++) {
+        char *pos = strstr(stringa, tipo[i]);                                                                     //Cerchiamo il tipo come sotto-stringa
 
-  //printf("%s\nLunghezza -> %d\n", finale,strlen(finale));                                                         // Qui abbiamo il nome della/delle variabili senza ne tipi ne uguale
-  //-----------------------------------------------------------------
+        //Se lo trova allora potrei far fare i-- , cosi che ricontrolli nuovamente per lo stesso tipo se ce ne è un'altra
+        if (pos != NULL) {
+          index = pos - stringa;                                                                                            //Calcoliamo l'indice sottraendo i puntatori
+          for (int j = 0; j < strlen(tipo[i]); j++) {
+            stringa[index++] = ' ';
+          }
+          i--;
+        }
+      }
 
-  char *token = strtok(finale, ",");                                                                              // Spezza la stringa una prima volta
-  while (token != NULL) {
-    variabili[index_variabili++] = token;
-    token = strtok(NULL, ",");
-    // Continua a spezzarla fino a quando non e' piu' possibile
-  }
+    char *finale = eliminaSpaziIniziali(stringa);
+    printf("%s\n", finale);
 
-     /*for (int i = 0; i<index_variabili; i++) {
-      printf("%s\n",variabili[i]);
-    } */
-
-  for (int i = 0; i < index_variabili; i++) {
-    variabili[i] = eliminaSpaziIniziali(variabili[i]);
-    if (isValidName(variabili[i])) {
-      printf("%s VALIDA\n", variabili[i]);
-    }else {
-      printf("%s NON-VALIDA\n", variabili[i]);
+    if (strchr(finale, '=')) {
+      for (int i = strlen(finale); finale[i] != '=' ; i--) {
+        finale[i] = ' ';
+        index = i;
+      }
+    }else{
+      index = strlen(finale);
     }
-  }
+    finale[index-1] = '\0';
+
+    char *token = strtok(finale, ",");                                                                              // Spezza la stringa una prima volta
+    while (token != NULL) {
+      token = extendedTrim(token);
+      token = charCleaner(token);                                                                                         // <- Può tornare utile per pulire eventuali ";" dalla fine del nome delle variabili
+      variabili[index_variabili++] = token;
+      token = strtok(NULL, ",");
+      // Continua a spezzarla fino a quando non e' piu' possibile
+    }
+
+    for (int i = 0; i < index_variabili; i++) {
+      variabili[i] = eliminaSpaziIniziali(variabili[i]);
+      if (isValidName(variabili[i])) {
+        printf("%s -> VALIDA\n", variabili[i]);
+      }else {
+        printf("%s -> NON-VALIDA\n", variabili[i]);
+      }
+    }
   }
   fclose(principale);
   return 0;
