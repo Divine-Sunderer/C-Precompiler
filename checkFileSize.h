9151db462aab4022c6483a3812c7dc5a706ae992
cristina <cripor004@gmail.com>
2025-05-27 17:20:41 +0200

    Delete checkFileSize.h

--- checkFileSize.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHECKFILESIZE_H
-#define CHECKFILESIZE_H
-#include <stdio.h>
-
-void checkFileLenght(FILE *input, FILE *statistiche, char *nome_input);
-void checkFileSize(FILE *input, FILE *statistiche, char *nome_input);
-
-#endif //CHECKFILESIZE_H
